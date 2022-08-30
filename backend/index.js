3cdb3b2a7ac7f04a21e198152f70ffc21282a708
BHANU PRATAP SINGH <bhanubhanu7737@gmail.com>
2022-08-30 23:38:03 +0530

    Backend-Login-Api-Add
    1. Make Backend API For User Log In.

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,39 @@
-const express = require( "express" );
-const cors = require( "cors" );
+const express = require("express");
+const cors = require("cors");
 
-require( "./db/config" );
-const User = require( "./db/User" );
+require("./db/config");
+const User = require("./db/User");
 const app = express();
 
-app.use( express.json() );
-app.use( cors() );
+app.use(express.json());
+app.use(cors());
 
-app.post( "/register", async ( req, resp ) => {
-    let user = new User( req.body );
+// Register Or add New User Api
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
     let result = await user.save();
-    resp.send( result );
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
 });
 
-app.listen( 5000 );
+// LogIN Api
+app.post("/login", async (req, resp) => {
+
+    if (req.body.password && req.body.email) {
+
+        let user = await User.findOne(req.body).select("-password");
+
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: "No User Found" });
+        }
+
+    } else {
+        resp.send({ result: "No User Found" });
+    }
+
+});
+
+app.listen(5000);
